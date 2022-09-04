c5edb329a91eea4b8ac0443dee233450f3cf7126
Иван Нигаев <Nihaiev@it.group>
2022-09-04 22:23:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ const createInfoMarkup = data => {
     ({ name, capital, population, flags, languages }) =>
       `<h1><img src="${flags.png}" alt="${
         name.official
-      }" width="40" height="40">${name.official}</h1>
+      }" width="60" height="40">${name.official}</h1>
       <p>Capital: ${capital}</p>
       <p>Population: ${population}</p>
       <p>Languages: ${Object.values(languages)}</p>`
